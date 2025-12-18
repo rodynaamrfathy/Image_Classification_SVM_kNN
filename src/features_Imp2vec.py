37c88d75ee8239a93d2e4cc0b5cb0f23ebf287f8
Rodyna Amr <130939400+rodynaamrfathy@users.noreply.github.com>
2025-12-19 01:28:17 +0200

    done

---
+++ src/features_Imp2vec.py
@@ -0,0 +1,48 @@
+import os
+import numpy as np
+from PIL import Image
+from img2vec_pytorch import Img2Vec
+from sklearn.preprocessing import LabelEncoder
+import joblib
+
+DATA_DIR = "/Users/rodynaamr/Image_Classification_SVM_kNN/data"
+SAVE_DIR = "models"
+
+os.makedirs(SAVE_DIR, exist_ok=True)
+
+img2vec = Img2Vec()
+
+X, y = [], []
+
+for class_name in sorted(os.listdir(DATA_DIR)):
+    class_path = os.path.join(DATA_DIR, class_name)
+
+    if not os.path.isdir(class_path):
+        continue
+    if class_name.endswith("_aug"):
+         continue
+
+    for img_name in os.listdir(class_path):
+        img_path = os.path.join(class_path, img_name)
+
+        try:
+            img = Image.open(img_path)
+            vec = img2vec.get_vec(img)
+            X.append(vec)
+            y.append(class_name)
+        except Exception as e:
+            print(f"Skipping {img_path}: {e}")
+
+X = np.array(X)
+
+# Encode labels
+le = LabelEncoder()
+y_encoded = le.fit_transform(y)
+
+np.save(os.path.join(SAVE_DIR, "features.npy"), X)
+np.save(os.path.join(SAVE_DIR, "labels.npy"), y_encoded)
+
+joblib.dump(le, os.path.join(SAVE_DIR, "label_encoder.pkl"))
+
+print("Features shape:", X.shape)
+print("Classes:", le.classes_)
